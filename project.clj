ab5595b48acacc29555e5c37bdf94317aecf68ce
Jeff Sack <jeff.sack@gmail.com>
2012-02-17 17:52:21 -0600

    further project setup, etc

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject playground "1.0.0-SNAPSHOT"
+  :description "demo of cascalog stuff"
+  :url "https://github.com/jeffsack/playground"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [cascalog "1.8.5"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
---
+++ test/playground/test/core.clj
@@ -0,0 +1,6 @@
+(ns playground.test.core
+  (:use [playground.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
