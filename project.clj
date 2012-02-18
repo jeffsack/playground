b5330c903934f2fb5510af18b78559720eb7b27f
Jeff Sack <jeff.sack@gmail.com>
2012-02-17 18:04:01 -0600

    adding cascading jdbc dep, updating .gitignore

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject playground "1.0.0-SNAPSHOT"
   :description "demo of cascalog stuff"
   :url "https://github.com/jeffsack/playground"
+  :repositories {"conjars" "http://conjars.org/repo/"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cascalog "1.8.5"]]
+                 [cascalog "1.8.5"]
+                 [r0man/cascading.jdbc "1.2"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
